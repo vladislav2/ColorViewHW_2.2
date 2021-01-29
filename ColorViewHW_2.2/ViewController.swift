27559ec12ebb6a0e445bfa935f7271cef5d88789
vladislav2 <etter.id75@gmail.com>
2021-01-29 23:04:05 +0200

    Initial Commit

---
+++ ColorViewHW_2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorViewHW_2.2
+//
+//  Created by user on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
